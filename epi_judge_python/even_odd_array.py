ed58646dc02fb8297d8e78e2cbebd5f16a47a2e8
Leon <lw75251@gmail.com>
2019-09-26 05:04:29 +1000

    Adds existing C++ solutions, and beginning of python

--- epi_judge_python/even_odd_array.py
+++ epi_judge_python/even_odd_array.py
@@ -5,10 +5,30 @@ from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
+# Your input is an array of
+# integers, and you have to reorder its entries so that the even entries appear first. This is easy if you
+# use O(n) space, where n is the length of the array. However, you are required to solve it without
+# allocating additional storage.
 
 def even_odd(A):
-    # TODO - you fill in here.
-    return
+    start, end = 0, len(A)-1
+    while start < end:
+        if A[start]%2 == 0:
+            start += 1
+        else:
+            temp = A[start]
+            A[start] = A[end]
+            A[end] = temp
+            end -= 1
+
+# def even_odd(A):
+#     next_even, next_odd = 0, len(A)-1
+#     while next_even < next_odd:
+#         if A[next_even] % 2 == 0:
+#             next_even += 1
+#         else:
+#             A[next_even], A[next_odd] = A[next_odd], A[next_even]
+#             next_odd -= 1
 
 
 @enable_executor_hook
