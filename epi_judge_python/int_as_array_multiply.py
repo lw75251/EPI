31dd842bb0a5d55967a7457656607d78957a531d
Leon <lw75251@gmail.com>
2019-09-27 07:27:32 +1000

    int_as_array_multiply solution

--- epi_judge_python/int_as_array_multiply.py
+++ epi_judge_python/int_as_array_multiply.py
@@ -2,9 +2,39 @@ from test_framework import generic_test
 
 
 def multiply(num1, num2):
-    # TODO - you fill in here.
-    return []
+    sign = -1 if (num1[0] < 0) ^ (num2[0] < 0) else 1
+    num1[0], num2[0] = abs(num1[0]), abs(num2[0])
 
+    result = [0] * (len(num1) + len(num2))
+    for i in reversed(range(len(num1))):
+        for j in reversed(range(len(num2))):
+            result[i+j+1] += num1[i] * num2[j]
+            result[i+j] += result[i+j+1] // 10
+            result[i+j+1] %= 10
+
+    i = 0
+    while i < len(result) and result[i] == 0:
+        i+= 1
+
+    result = [0] if i == len(result) else result[i:]
+
+    return [sign * result[0]] + result[1:]
+
+# def multiply(num1, num2):
+#     sign = -1 if ( num1[0] < 0 ) ^ (num2[0] < 0) else 1
+#     num1[0], num2[0] = abs(num1[0]), abs(num2[0])
+
+#     result = [0] * (len(num1) + len(num2))
+#     for i in reversed(range(len(num1))):
+#         for j in reversed(range(len(num2))):
+#             result[i+j+1] += num1[i] * num2[j]
+#             result[i+j] += result[i+j+1] // 10
+#             result[i+j+1] %= 10
+    
+#     result = result[next((i for i, x in enumerate(result)
+#                          if x != 0), len(result)):] or [0]
+    
+#     return [sign * result[0]] + result[1:]
 
 if __name__ == '__main__':
     exit(
